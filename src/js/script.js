ef51b18d3ef551178c93d93934344f6d67d80eaf
Igor Freitas <igorlo.freitas@hotmail.com>
2023-10-19 12:57:05 -0300

    JS image adjust

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,15 @@ function toggleMode() {
     const html = document.documentElement
     html.classList.toggle("light")
 
+    // pegar a tag img
     const img = document.querySelector("#profile img")
 
+    // substituir a imagem
     if (html.classList.contains("light")) {
-        img.setAttribuite("src", "/src/imagens/assets/sunglasses.png")
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "/src/imagens/assets/sunglasses.png")
     } else {
-        img.setAttribuite("src", "/src/imagens/assets/perfil (1).png")
+        // set tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "/src/imagens/assets/perfil (1).png")
     }
-
-
-
 }
